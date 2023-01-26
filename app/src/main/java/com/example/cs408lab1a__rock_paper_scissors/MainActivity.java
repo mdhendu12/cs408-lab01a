45b0e54e3373eb8d249d22e08afd676ab1d97509
matthew <mdhendu12@gmail.com>
2023-01-26 14:10:29 -0600

    Hotfixes

--- app/src/main/java/com/example/cs408lab1a__rock_paper_scissors/MainActivity.java
+++ app/src/main/java/com/example/cs408lab1a__rock_paper_scissors/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.cs408lab1a__rock_paper_scissors;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs408lab1a__rock_paper_scissors.databinding.ActivityMainBinding;
 
